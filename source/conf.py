290d8630c8fff15139980fdcd050e1e88b19ab1e
Takafumi Arakaki <aka.tkf@gmail.com>
2015-04-13 21:28:01 +0200

    Use html_theme = 'haiku' for now

--- source/conf.py
+++ source/conf.py
@@ -112,7 +112,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -157,13 +157,13 @@ html_theme = 'alabaster'
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    '**': [
-        'about.html', 'navigation.html',
-        'sourcelink.html', 'searchbox.html',
-        # 'donate.html',
-    ],
-}
+# html_sidebars = {
+#     '**': [
+#         'about.html', 'navigation.html',
+#         'sourcelink.html', 'searchbox.html',
+#         # 'donate.html',
+#     ],
+# }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
